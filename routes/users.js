8af6a3c3bff82b59f6e268b3c49c46409baae249
laugesen <amir.laugesen@student.vinci.be>
2024-10-10 16:14:55 +0200

    refractor : FindUserDB

--- routes/users.js
+++ routes/users.js
@@ -18,10 +18,7 @@ router.get('/', (req, res, next) => {
 
 /* check login and password */
 router.post('/login', (req, res, next) => {
-    console.log("USERS LOGIN");
-    // User in DB ? -> return the record of the user if found
-    const userFound = User.find(req.body.userLogin);
-    console.log("User found" + JSON.stringify(userFound));
+    const userFound = FindUserInDB(req);
     if (userFound) {
         if (userFound.active == false) {
             AccountDeactivated(req, res);
@@ -91,6 +88,14 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function FindUserInDB(req) {
+    console.log("USERS LOGIN");
+    // User in DB ? -> return the record of the user if found
+    const userFound = User.find(req.body.userLogin);
+    console.log("User found" + JSON.stringify(userFound));
+    return userFound;
+}
 
 function CorrectPassword(req) {
     console.log("password correct");
